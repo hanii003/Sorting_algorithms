a6ca8be265967ac1efe19dce691ef4915de22e05
hanii003 <138033876+hanii003@users.noreply.github.com>
2024-07-06 23:20:21 +0330

    Add files via upload

---
+++ merge_sort.cpp
@@ -0,0 +1,85 @@
+Here is an implementation of the merge sort algorithm in C++ for sorting numerical data:
+
+```cpp
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void merge(vector<int>& arr, int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    vector<int> L(n1);
+    vector<int> R(n2);
+
+    for (int i = 0; i < n1; i++) {
+        L[i] = arr[l + i];
+    }
+    for (int j = 0; j < n2; j++) {
+        R[j] = arr[m + 1 + j];
+    }
+
+    int i = 0;
+    int j = 0;
+    int k = l;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(vector<int>& arr, int l, int r) {
+    if (l >= r) {
+        return;
+    }
+
+    int m = l + (r - l) / 2;
+    mergeSort(arr, l, m);
+    mergeSort(arr, m + 1, r);
+
+    merge(arr, l, m, r);
+}
+
+void printArray(vector<int>& arr) {
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    vector<int> nums = {12, 11, 13, 5, 6, 7};
+    
+    cout << "Original array:" << endl;
+    printArray(nums);
+
+    mergeSort(nums, 0, nums.size() - 1);
+
+    cout << "Sorted array:" << endl;
+    printArray(nums);
+
+    return 0;
+}
+```
+
+You can modify this code to sort textual data (strings) by changing the data type from `int` to `string` and adjusting the input dataset accordingly.
